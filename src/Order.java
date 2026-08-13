c8c85427ea85a3380f2f79dad47b0e32f6d97f35
Angeline Gurung <angelinegurung@gmail.com>
2026-08-13 17:05:53 +0100

    implement Order class and OrderBook map structure

--- src/Order.java
+++ src/Order.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Order {
+
+    String side;
+    String orderId;
+    Double price;
+    int quantity;
+
+    public Order(String side, String orderId, Double price, int quantity) {
+        this.side = side;
+        this.orderId = orderId;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
