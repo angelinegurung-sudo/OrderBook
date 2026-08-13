a66f17195fe2b1e1b292114c8d0bf243cc543f4c
Angeline Gurung <angelinegurung@gmail.com>
2026-08-13 17:03:59 +0100

    fix type mismatch in OrderBook constructor

---
+++ src/OrderBook.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class OrderBook {
+   private HashMap<String, Order> orders;
+
+   public OrderBook() {
+       this.orders = new HashMap<>();
+   }
+
+
+
+
+
+}
